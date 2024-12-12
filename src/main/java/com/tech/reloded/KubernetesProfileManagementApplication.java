b12748dd4d4e452388c9692ee1e231fbd219f6d0
Gaurav kumar <gaurav@Gauravs-MacBook-Pro.local>
2024-12-12 19:31:39 +0530

    project initialization

---
+++ src/main/java/com/tech/reloded/KubernetesProfileManagementApplication.java
@@ -0,0 +1,13 @@
+package com.tech.reloded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesProfileManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesProfileManagementApplication.class, args);
+	}
+
+}
